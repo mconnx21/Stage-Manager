3f2e09c9d3812d68147c1f8b25dd67a9ddd7e4b8
mconnx21 <mirandaconn1@gmail.com>
2024-10-24 11:28:25 +0100

    Added DanceNodes and PersonNodes

---
+++ DanceNode.py
@@ -0,0 +1,7 @@
+class DanceNode:
+
+    def __init__(self, id, name, style, category):
+        self.id = id
+        self.name = name
+        self.style = style
+        self.category = category 
---
+++ PersonNode.py
@@ -0,0 +1,5 @@
+class PersonNode:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
